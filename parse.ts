c867eea6f03e6ac6559a04a7b22f492e7322fcf5
Dmitry256 <pustovalov.dimaz@gmail.com>
2024-06-17 00:26:56 +0300

    playwright-extra + stealth plugin method wip

--- parse.ts
+++ parse.ts
@@ -10,7 +10,7 @@ const crawler = new PlaywrightCrawler({
             timeout: 60000,
         });
         await page.waitForTimeout(5000);
-        const htmlSource: string  = await page.content();
+        const htmlSource: string = await page.content();
         const fullPageElement = parse(htmlSource);
         const tableSource = fullPageElement.querySelector('.span8 .table.table-bordered.table-hover.table-responsive-flex');
         const extractedTableContent = extractTableContent(tableSource);
@@ -32,13 +32,13 @@ function extractTableContent(tableSource: HTMLElement | null): Record<string, an
             let value: any = parseStringOrNumber(cells[1].innerText.trim());
 
             if (isString(value)) { value = parseStringOrDate(value) };
-            
+
             if (isString(value)) {
                 const linkElement = cells[1].querySelector('a');
-                if (linkElement) { value = linkElementParse(linkElement);}
+                if (linkElement) { value = linkElementParse(linkElement); }
                 else {
                     const osIcons = cells[1].querySelectorAll('svg');
-                    if (osIcons.length > 0) { value = supportedSystemsElementParse(cells[1])};
+                    if (osIcons.length > 0) { value = supportedSystemsElementParse(cells[1]) };
                 };
             };
             tableContent[key] = value;
@@ -53,7 +53,7 @@ function supportedSystemsElementParse(supportedSystemsCell: HTMLElement): string
         .filter(node => node.nodeType === NodeType.TEXT_NODE)
         .map(node => node.textContent?.trim())
         .filter(text => text);
-    if (steamDeckPlayable.length > 0) {systemsArray.push('Steam Deck')};
+    if (steamDeckPlayable.length > 0) { systemsArray.push('Steam Deck') };
     return systemsArray
 }
 
@@ -70,14 +70,14 @@ function isString(value: any): boolean {
 
 function parseStringOrNumber(inputString: string): string | number {
     const number = Number(inputString)
-    if (isNaN(number)) return inputString; 
+    if (isNaN(number)) return inputString;
     else return number
 };
 
 function parseStringOrDate(inputString: string): string | Date {
     if (inputString.search(/\d{2} \w{3,9} \d{4} – \d{2}:\d{2}:\d{2} (UTC)/) == 0) {
         const dateArray = inputString.split(' ');
-        return new Date(`${ dateArray[1] } ${ dateArray[0] }, ${ dateArray[2] } ${ dateArray[4] } ${ dateArray[5] }`);
+        return new Date(`${dateArray[1]} ${dateArray[0]}, ${dateArray[2]} ${dateArray[4]} ${dateArray[5]}`);
     } else return inputString
 };
 
