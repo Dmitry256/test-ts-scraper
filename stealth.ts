798bea1e692b6d7cb567475b7ffe3f5ae7f9da4f
Dmitry256 <pustovalov.dimaz@gmail.com>
2024-06-17 12:28:18 +0300

    add fetchGameData() in stealth

--- stealth.ts
+++ stealth.ts
@@ -6,10 +6,9 @@ import { readFile, writeFile } from 'fs/promises';
 import { parse } from 'node-html-parser';
 import { extractTableContent } from "./utils"
 
-
 chromium.use(StealthPlugin());
 
-(async () => {
+async function fetchGameData(url: string) {
     const browser = await chromium.launch({ headless: true });
     const context = await browser.newContext({
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
@@ -20,18 +19,17 @@ chromium.use(StealthPlugin());
     const page = await context.newPage();
 
     console.log("Parsing game data with stealth plugin..");
-    await page.goto("https://steamdb.info/app/730/charts/", { waitUntil: "domcontentloaded" });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
     await page.waitForTimeout(5000)
-    // await page.screenshot({ path: "screenshot.png", fullPage: true });
     const htmlSource: string = await page.content();
     const fullPageElement = parse(htmlSource);
     const tableSource = fullPageElement.querySelector('.span8 .table.table-bordered.table-hover.table-responsive-flex');
     const extractedTableContent = extractTableContent(tableSource);
     await saveData('gameData.json', extractedTableContent);
     await readFileToLog();
-    console.log("All done, check the screenshot. ✨");
+    console.log("All done, check the file: storage/gameData.json. ✨");
     await browser.close();
-})();
+};
 
 async function saveData(filename: string, data: any) {
     if (!existsSync(resolve(__dirname, 'storage'))) {
@@ -49,3 +47,4 @@ async function readFileToLog() {
     )
     )
 };
+fetchGameData('http://steamdb.info/app/730/charts/')
